bda0668afb1832c4553c2e479f3dbccb29967810
Aanand Prasad <aanand.prasad@gmail.com>
2012-07-12 17:20:47 +0100

    Test with mocha

--- test/monads.js
+++ test/monads.js
@@ -1,29 +1,51 @@
-var doNotation = require('../do-notation');
+var assert     = require('assert'),
+    doNotation = require('../do-notation');
 
-var bindIdentity = function(obj, fn) { return fn(obj) };
-var bindMaybe    = function(obj, fn) { return obj && fn(obj) };
-var bindList     = function(arr, fn) { return [].concat.apply([], arr.map(fn)) }
+describe('the identity monad', function() {
+  var bind = function(obj, fn) { return fn(obj) };
 
-console.log(doNotation.run(bindIdentity, function() {
-  var x = 1;
-  var y = 2;
-  return x + y;
-}) === 3);
+  it('behaves like normal variable assignment', function() {
+    assert.equal(doNotation.run(bind, function() {
+      var x = 1;
+      var y = 2;
+      return x + y;
+    }), 3)
+  })
+})
 
-console.log(doNotation.run(bindMaybe, function() {
-  var x = 1;
-  var y = 2;
-  return x + y;
-}) === 3);
+describe('the maybe monad', function() {
+  var bind = function(obj, fn) { return obj && fn(obj) };
 
-console.log(doNotation.run(bindMaybe, function() {
-  var x = 1;
-  var y = null;
-  throw "error";
-}) === null);
+  it('behaves normally when there are no nulls', function() {
+    assert.equal(doNotation.run(bind, function() {
+      var x = 1;
+      var y = 2;
+      return x + y;
+    }), 3);
+  });
 
-console.log(doNotation.run(bindList, function() {
-  var x = ["first", "second"];
-  var y = ["once", "twice"];
-  return [x + " cousin " + y + " removed "];
-}));
+  it('short-circuits when it encounters a null value', function() {
+    assert.equal(doNotation.run(bind, function() {
+      var x = 1;
+      var y = null;
+      throw "error";
+    }), null);
+  });
+})
+
+describe('the list monad', function() {
+  var bind = function(arr, fn) { return [].concat.apply([], arr.map(fn)) };
+
+  it('returns all combinations', function() {
+    assert.deepEqual(doNotation.run(bind, function() {
+      var x = ["first", "second"];
+      var y = ["once", "twice"];
+      return [x + " cousin " + y + " removed"];
+    }), [
+      "first cousin once removed",
+      "first cousin twice removed",
+      "second cousin once removed",
+      "second cousin twice removed"
+    ]);
+  })
+})
